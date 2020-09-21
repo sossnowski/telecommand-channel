6d864b4608f0cac5a0bdb5d878347e2906babed8
Krzysztof Brzoska <kbrzoska97@gmail.com>
2020-09-21 19:24:01 +0200

    data flow decode added

--- dataFlowVariant.c
+++ dataFlowVariant.c
@@ -22,6 +22,25 @@ char* normalToNRZM(char* data) {
     return resultSignal;
 }
 
+char* NRZMtoNormal(char* data) {
+    int dataLength = strlen(data);
+    char* resultSignal = malloc(sizeof(char) * dataLength);
+    if (data[0] == '1')
+        resultSignal[0] = '1';
+    else
+        resultSignal = '0';
+    for (int i = 1; i < dataLength - 1; ++i) {
+        if (data[i] == '1' && resultSignal[i-1] == '1')
+            resultSignal[i] = '0';
+        else if (data[i] == '1' && resultSignal[i-1] == '0')
+            resultSignal[i] = '1';
+        else if (data[i] == '0')
+            resultSignal[i] = resultSignal[i-1];
+    }
+
+    return resultSignal;
+}
+
 char* normalToSPL(char* data) {
     int dataLength = strlen(data);
     char* resultData = malloc(sizeof(char) * dataLength * 2);
@@ -36,4 +55,18 @@ char* normalToSPL(char* data) {
         }
     }
     return resultData;
+}
+
+char* SPLToNormal(char* data) {
+    int dataLength = strlen(data);
+    char* resultSignal = malloc(sizeof(char) * dataLength / 2);
+    for (int i = 0; i < dataLength; ++i) {
+        if (data[i] == '1' && data[i + 1] == '0')
+            resultSignal[i / 2] = '1';
+        else
+            resultSignal[i / 2] = '0';
+
+        ++i;
+    }
+    return resultSignal;
 }
--- dataFlowVariant.h
+++ dataFlowVariant.h
@@ -2,6 +2,8 @@
 #define BCH_DATAFLOWVARIANT_H
 
 char* normalToNRZM(char* data);
+char* NRZMToNormal(char* data);
 char* normalToSPL(char* data);
+char* SPLToNormal(char* data);
 
 #endif //BCH_DATAFLOWVARIANT_H
